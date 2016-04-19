7d1750e534cd7f032adef16b61e19b125a8a20bf
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-04-19 04:33:07 +0200

    add protobuf-net defs

---
+++ cfgrec/DataExtraction.fs
@@ -0,0 +1,36 @@
+﻿namespace cfgrecon
+  module DataExtraction =
+
+    type architecture_t =
+      | X86 = 0
+      | X86_64 = 1
+
+    type typeid_address_t =
+      | Bit32 = 0
+      | Bit64 = 1
+
+    type value_address_t =
+      | Value_32 of uint32
+      | Value_64 of uint64
+
+    type address_t =
+      { typeid: typeid_address_t;
+        value: value_address_t }
+
+    type header_t =
+      { architecture: architecture_t;
+        address_size: typeid_address_t }
+
+    type register_t =
+      { name: string;
+        value: address_t }
+
+    type memory_t =
+      { address: address_t;
+        value: address_t }
+
+    type typeid_con_info_t =
+      | RegRead = 0
+      | RegWrite = 1
+      | MemLoad = 2
+      | MemStore = 3
---
+++ cfgrec/Instruction.fs
@@ -0,0 +1,49 @@
+﻿namespace cfgrecon
+  module Instruction =
+    type Addrint = uint32
+    type MemoryMap = Map<Addrint, Addrint>
+    type RegisterMap = Map<string, Addrint>
+    type Instruction = { address : Addrint;
+                         read_registers : RegisterMap;
+                         written_registers : MemoryMap;
+                         read_addresses : MemoryMap;
+                         written_addresses : MemoryMap }
+
+    let make_instruction ins_addr read_regs written_regs read_addrs written_addrs =
+       { address = ins_addr;
+         read_registers = read_regs;
+         written_registers = written_regs;
+         read_addresses = read_addrs;
+         written_addresses = written_addrs }
+
+    let get_read_register_value reg_name ins =
+      match ins with
+        | { address = _;
+            read_registers = regs;
+            written_registers = _;
+            read_addresses = _;
+            written_addresses = _ } -> Map.find reg_name regs
+
+    let get_written_register_value reg_name ins =
+      match ins with
+        | { address = _;
+            read_registers = _;
+            written_registers = regs;
+            read_addresses = _;
+            written_addresses = _ } -> Map.find reg_name regs
+
+    let get_read_memory_value mem_addr ins =
+      match ins with
+        | { address = _;
+            read_registers = _;
+            written_registers = _;
+            read_addresses = addrs;
+            written_addresses = _ } -> Map.find mem_addr addrs
+
+    let get_written_memory_value mem_addr ins =
+      match ins with
+        | { address = _;
+            read_registers = _;
+            written_registers = _;
+            read_addresses = _;
+            written_addresses = addrs } -> Map.find mem_addr addrs
