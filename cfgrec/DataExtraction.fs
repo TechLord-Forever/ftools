0cb0b075137ab203a904f448ca8047bcda869c94
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-04-21 02:27:27 +0200

    parse header

--- cfgrec/DataExtraction.fs
+++ cfgrec/DataExtraction.fs
@@ -4,34 +4,40 @@
       | X86
       | X86_64
 
-    type RegisterAccess<'T> = { name: string; value: 'T }
-    type MemoryAccess<'T> = { address: 'T; value: 'T }
-
+    // type RegisterAccess<'T> = { name: string; value: 'T }
+    // type MemoryAccess<'T> = { address: 'T; value: 'T }
 
     type MachineInfo = { arch: MachineArchitecture;
                          address_size: uint32 }
 
-    type Instruction<'T> = { address: 'T;
-                             disassemble: string;
-                             thread_id: uint32;
-                             opcode: byte[];
-                             read_registers: RegisterAccess list;
-                             write_registers: RegisterAccess list;
-                             read_addresses: MemoryAccess list;
-                             write_addresses: MemoryAccess list }
+    // type Instruction<'T> = { address: 'T;
+    //                          disassemble: string;
+    //                          thread_id: uint32;
+    //                          opcode: byte[];
+    //                          read_registers: RegisterAccess list;
+    //                          write_registers: RegisterAccess list;
+    //                          read_addresses: MemoryAccess list;
+    //                          write_addresses: MemoryAccess list }
 
     (*==========================================================================*)
 
     let private extract_packed_data (reader:System.IO.BinaryReader) =
-      use data_size = reader.ReadUInt32 ()
-      reader.ReadBytes (int data_size)
+      reader.ReadBytes (int (reader.ReadUInt32()))
 
     let get_machine_information (data: byte[]) =
-      
+      let json_header = Chiron.Parsing.Json.parse (string data)
+      let header_map:Map<string, obj> = unbox json_header
+      let header_arch = System.Decimal.ToUInt32 (unbox<decimal> <| Map.find "architecture" header_map)
+      let header_addr_size = System.Decimal.ToUInt32 (unbox<decimal> <| Map.find "address size" header_map)
+      match (int header_arch) with
+        | 0 -> { arch = X86; address_size = header_addr_size }
+        | 1 -> { arch = X86_64; address_size = header_addr_size }
+        | _ -> failwith "unknown architecture"
 
-    let get_instructions (data: byte[]) =
-      use data_as_string = System.String (data)
-      use json_instructions = Chiron.Json.parse data_as_string
+
+    // let get_instructions (data: byte[]) =
+    //   use data_as_string = (string data)
+    //   use json_instructions = Chiron.Json.parse data_as_string
 
 
 
--- cfgrec/Instruction.fs
+++ cfgrec/Instruction.fs
@@ -1,49 +1,28 @@
 ﻿namespace cfgrecon
   module Instruction =
-    type Addrint = uint32
-    type MemoryMap = Map<Addrint, Addrint>
-    type RegisterMap = Map<string, Addrint>
-    type Instruction = { address : Addrint;
-                         read_registers : RegisterMap;
-                         written_registers : MemoryMap;
-                         read_addresses : MemoryMap;
-                         written_addresses : MemoryMap }
+    type MemoryMap<'T when 'T : comparison> = Map<'T, 'T>
 
-    let make_instruction ins_addr read_regs written_regs read_addrs written_addrs =
-       { address = ins_addr;
-         read_registers = read_regs;
+    type RegisterMap<'T> = Map<string, 'T>
+
+    type Instruction<'T when 'T : comparison> =
+      { address           : 'T;
+        disassemble       : string;
+        thread_id         : uint32;
+        opcode            : byte[];
+        read_registers    : RegisterMap<'T>;
+        written_registers : RegisterMap<'T>;
+        read_addresses    : MemoryMap<'T>;
+        written_addresses : MemoryMap<'T> }
+
+    let make_instruction (ins_addr, ins_disas, ins_thread_id, ins_opcode,
+                          read_regs, written_regs, read_addrs, written_addrs) =
+       { address           = ins_addr;
+         disassemble       = ins_disas;
+         thread_id         = ins_thread_id;
+         opcode            = ins_opcode;
+         read_registers    = read_regs;
          written_registers = written_regs;
-         read_addresses = read_addrs;
+         read_addresses    = read_addrs;
          written_addresses = written_addrs }
 
-    let get_read_register_value reg_name ins =
-      match ins with
-        | { address = _;
-            read_registers = regs;
-            written_registers = _;
-            read_addresses = _;
-            written_addresses = _ } -> Map.find reg_name regs
 
-    let get_written_register_value reg_name ins =
-      match ins with
-        | { address = _;
-            read_registers = _;
-            written_registers = regs;
-            read_addresses = _;
-            written_addresses = _ } -> Map.find reg_name regs
-
-    let get_read_memory_value mem_addr ins =
-      match ins with
-        | { address = _;
-            read_registers = _;
-            written_registers = _;
-            read_addresses = addrs;
-            written_addresses = _ } -> Map.find mem_addr addrs
-
-    let get_written_memory_value mem_addr ins =
-      match ins with
-        | { address = _;
-            read_registers = _;
-            written_registers = _;
-            read_addresses = _;
-            written_addresses = addrs } -> Map.find mem_addr addrs
