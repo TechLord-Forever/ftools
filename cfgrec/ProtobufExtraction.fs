9377ca264531f6153875bef094a0507fa689c380
Ta Thanh Dinh <tathanhdinh@gmail.com>
2016-04-26 16:18:50 +0200

    How to dehydrating unsigned types !?

--- cfgrec/ProtobufExtraction.fs
+++ cfgrec/ProtobufExtraction.fs
@@ -1,3 +1,26 @@
 ﻿namespace cfgrecon
   module ProtobufExtraction =
 
+    // architecture_t
+    type Architecture =
+      X86 = 0 | X86_64 = 1
+
+    // typeid_address_t
+    type TypeIdOfAddress =
+      Bit32 = 0 | Bit64 = 1
+
+    // value_address_t
+    type ValueOfAddress () =
+      inherit Froto.Core.Encoding.MessageBase()
+
+      let m_value_32 = ref (uint32 0)
+      let m_value_64 = ref (uint64 0)
+
+      override x.Clear() =
+        m_value_32 := (uint32 0)
+        m_value_64 := (uint64 0)
+
+      override x.Encode(zc_buffer) =
+        let encode =
+          (!m_value_32 |> Froto.Core.Encoding.Serializer.de)
+      
